e0bc67357097f74369d7ade5f09f2cf4e4f6ce85
Junko Tahara <junkotahara911@gmail.com>
2020-03-16 15:58:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash[key]
+  hash[key] if hash[key]
+  nil
 end
 
 def update_counting_hash(hash, key)
